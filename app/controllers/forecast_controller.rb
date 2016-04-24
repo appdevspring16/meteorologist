a44b762a2f6d7c6f08aa3cfed46078bb6d96f06f
Matthew Keenan <mkeenan@chicagobooth.edu>
2016-04-23 21:17:30 -0500

    Forecast fix

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -19,7 +19,7 @@ class ForecastController < ApplicationController
     ds_api = "https://api.forecast.io/forecast/"
     api_key = "3b8fdec05259d0f35ae3b0fe32d31cc2"
     loc = "/#{@lat},#{@lng}"
-    ds_url = ds_loc + api_key + loc
+    ds_url = ds_api + api_key + loc
 
     raw_data = open(ds_url).read
     parsed_data = JSON.parse(raw_data)
