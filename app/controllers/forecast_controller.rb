a39b9b0e4e731a296949fc64283f905bc14bbe3d
mmuntazir <mehdi.muntazir@outlook.com>
2016-04-24 00:27:24 -0500

    Basic version complete

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -7,27 +7,31 @@ class ForecastController < ApplicationController
   end
 
   def coords_to_weather
-    @lat = params[:user_latitude]
-    @lng = params[:user_longitude]
+    @latitude = params[:user_latitude]
+    @longitude = params[:user_longitude]
 
     # ==========================================================================
     # Your code goes below.
     # The latitude the user input is in the string @lat.
     # The longitude the user input is in the string @lng.
     # ==========================================================================
+    url = 'https://api.forecast.io/forecast/ef03e0e63c842a7e051dee20d9041d85/' + @lat + ","+@lng
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
 
 
 
-    @current_temperature = "Replace this string with your answer."
+    @current_temperature = parsed_data["currently"]["temperature"]
 
-    @current_summary = "Replace this string with your answer."
+    @current_summary = parsed_data["currently"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_data["minutely"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_data["hourly"]["summary"]
 
-    @summary_of_next_several_days = "Replace this string with your answer."
-
+    @summary_of_next_several_days = parsed_data["daily"]["summary"]
+    @latitude = @latitude
+    @longitude = @longitude
     render("coords_to_weather.html.erb")
   end
 end
--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -16,18 +16,24 @@ class MeteorologistController < ApplicationController
     # A URL-safe version of the street address, with spaces and other illegal
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
+    url = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + url_safe_street_address
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+    url2 = "https://api.forecast.io/forecast/ef03e0e63c842a7e051dee20d9041d85/"+"#{@latitude}"+","+"#{@longitude}"
+    raw_data = open(url2).read
+    parsed_data = JSON.parse(raw_data)
 
+    @current_temperature = parsed_data["currently"]["temperature"]
 
+    @current_summary = parsed_data["currently"]["summary"]
 
-    @current_temperature = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_data["minutely"]["summary"]
 
-    @current_summary = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_data["hourly"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
-
-    @summary_of_next_several_hours = "Replace this string with your answer."
-
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = parsed_data["daily"]["summary"]
 
     render("street_to_weather.html.erb")
   end
