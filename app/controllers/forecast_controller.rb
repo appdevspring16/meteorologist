c47c26cff7fa39acf51799e5662307100d261022
alathans <alathans@users.noreply.github.com>
2016-04-24 16:45:26 -0500

    added forecasting

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -16,17 +16,19 @@ class ForecastController < ApplicationController
     # The longitude the user input is in the string @lng.
     # ==========================================================================
 
+url_safe_full = "https://api.forecast.io/forecast/e308f111cdb2db5d0a337db32f008c75/#{@lat},#{@lng}"
+parsed_data = JSON.parse(open(url_safe_full).read)
 
+    @current_temperature = parsed_data["currently"]["temperature"]
 
-    @current_temperature = "Replace this string with your answer."
+    @current_summary =
+    parsed_data["currently"]["summary"]
 
-    @current_summary = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_data["minutely"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_data["hourly"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
-
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = parsed_data["daily"]["summary"]
 
     render("coords_to_weather.html.erb")
   end
