2848400407211683c228fb214a54623034700c40
shao313 <shaozhu@chicagobooth.edu>
2016-04-24 18:08:24 -0500

    weather

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -16,17 +16,22 @@ class ForecastController < ApplicationController
     # The longitude the user input is in the string @lng.
     # ==========================================================================
 
+current_time = Time.current.to_i
+
+parsed_data_current = JSON.parse(open("https://api.forecast.io/forecast/490fb662acbfe54e362a09de357d59ac/#{@lat},#{@lng}").read)
+
+parsed_data_hour = JSON.parse(open("https://api.forecast.io/forecast/490fb662acbfe54e362a09de357d59ac/#{@lat},#{@lng},#{current_time+3600}").read)
 
 
-    @current_temperature = "Replace this string with your answer."
+    @current_temperature = parsed_data_current["currently"]["temperature"]
 
-    @current_summary = "Replace this string with your answer."
+    @current_summary = parsed_data_current["currently"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_data_hour["currently"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_data_current["hourly"]["summary"]
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = parsed_data_current["daily"]["summary"]
 
     render("coords_to_weather.html.erb")
   end
