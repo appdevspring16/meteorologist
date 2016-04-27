6c772237b5095b92d703f74841567e5cbec06e1b
Josh Charney <jfcharney@user.noreply.github.com>
2016-04-27 00:06:39 -0500

    done
    Ready for feedback

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -19,9 +19,14 @@ class GeocodingController < ApplicationController
 
 
 
-    @latitude = "Replace this string with your answer."
+    # @latitude = "Replace this string with your answer."
 
-    @longitude = "Replace this string with your answer."
+    parsed_data = JSON.parse(open('https://maps.googleapis.com/maps/api/geocode/json?address='+ url_safe_street_address).read)
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+
+
+    # @longitude = "Replace this string with your answer."
 
     render("street_to_coords.html.erb")
   end
