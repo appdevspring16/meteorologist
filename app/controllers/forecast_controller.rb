1b682eae435525c6e8299529e8710a9fdb08e75a
Aaron Goltz <aarongoltz@users.noreply.github.com>
2016-04-29 13:38:54 -0500

    Matching the full specifications of the target.

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -1,4 +1,5 @@
 require 'open-uri'
+require 'JSON'
 
 class ForecastController < ApplicationController
   def coords_to_weather_form
@@ -16,17 +17,19 @@ class ForecastController < ApplicationController
     # The longitude the user input is in the string @lng.
     # ==========================================================================
 
+    raw_forecast_data = open("https://api.forecast.io/forecast/6db586fa317d71790b61def2233b3d83/" + @lat + ","+ @lng).read
 
+    parsed_forecast_data = JSON.parse(raw_forecast_data)
 
-    @current_temperature = "Replace this string with your answer."
+    @current_temperature = parsed_forecast_data["currently"]["temperature"]
 
-    @current_summary = "Replace this string with your answer."
+    @current_summary = parsed_forecast_data["currently"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_forecast_data["minutely"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_forecast_data["hourly"]["summary"]
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = parsed_forecast_data["daily"]["summary"]
 
     render("coords_to_weather.html.erb")
   end
--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -17,17 +17,35 @@ class MeteorologistController < ApplicationController
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
 
+    raw_data = open("http://maps.googleapis.com/maps/api/geocode/json?address=" +url_safe_street_address).read
 
+    parsed_data = JSON.parse(raw_data)
 
-    @current_temperature = "Replace this string with your answer."
+    results = parsed_data["results"]
 
-    @current_summary = "Replace this string with your answer."
+    first = results[0]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    geometry = first["geometry"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    location = geometry["location"]
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    latitude = location["lat"]
+
+    longitude = location["lng"]
+
+    raw_forecast_data = open("https://api.forecast.io/forecast/6db586fa317d71790b61def2233b3d83/" + latitude.to_s + ","+ longitude.to_s).read
+
+    parsed_forecast_data = JSON.parse(raw_forecast_data)
+
+    @current_temperature = parsed_forecast_data["currently"]["temperature"]
+
+    @current_summary = parsed_forecast_data["currently"]["summary"]
+
+    @summary_of_next_sixty_minutes = parsed_forecast_data["minutely"]["summary"]
+
+    @summary_of_next_several_hours = parsed_forecast_data["hourly"]["summary"]
+
+    @summary_of_next_several_days = parsed_forecast_data["daily"]["summary"]
 
     render("street_to_weather.html.erb")
   end
