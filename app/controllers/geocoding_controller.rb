62cda55fbb488290279dd173fdcb60abd53f128f
Zachary C. Cornett <zcornett@chicagobooth.edu>
2016-04-29 19:39:12 -0500

    Meteorologist_submission

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -17,11 +17,13 @@ class GeocodingController < ApplicationController
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
 
+require 'open-uri'
+url="http://maps.googleapis.com/maps/api/geocode/json?address="+ url_safe_street_address
 
-
-    @latitude = "Replace this string with your answer."
-
-    @longitude = "Replace this string with your answer."
+open(url)
+parsed_data=JSON.parse(open(url).read)
+@latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
     render("street_to_coords.html.erb")
   end
